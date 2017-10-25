58ffd6dea16606223b752f6234ff2b68c5b3ac8a
Ben Greene <bgree052@gold.ac.uk>
2017-10-25 19:22:22 +0100

    Initial Commit

---
+++ DynamicArray.cpp
@@ -0,0 +1,40 @@
+#include "DynamicArray.hpp"
+
+DynamicArray::DynamicArray(size_t size) {
+	storage = new int[size];
+	nitems = 0;
+}
+
+size_t DynamicArray::length() {
+  return (size_t) nitems;
+}
+
+int DynamicArray::select(size_t k) {
+	return storage[k];
+}
+
+void DynamicArray::store(int o, size_t k) {
+	storage[k] = o;
+}
+
+void DynamicArray::push(int o) {
+	if (nitems > storage_length) {
+		extend();
+	}
+	storage[nitems] = o;
+	nitems++;
+}
+
+int DynamicArray::pop() {
+	nitems--;
+	return storage[nitems];
+}
+
+void DynamicArray::extend() {
+	//new int = newLength;
+	//int newLength = storage_length++;
+	//for (int i = 0; i < storage_length; i++) {
+
+	//}
+
+}
